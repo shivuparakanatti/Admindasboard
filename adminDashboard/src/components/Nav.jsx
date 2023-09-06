42aeaaa5304b60fd00fcd40e774aa3e4d23a9fcd
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-09-06 21:58:57 +0530

    Update Nav.jsx

--- adminDashboard/src/components/Nav.jsx
+++ adminDashboard/src/components/Nav.jsx
@@ -11,7 +11,6 @@ import { useStateContext } from "../contexts/ContextProvider"
 import { Link } from "react-router-dom"
 
 
-
 const Nav = () => {
   
     const [isActive, setIsActive] = useState(false)
