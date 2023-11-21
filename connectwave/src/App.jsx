b734c0259a450c36ac794a60cafe93f5d3e3b2a8
Your Name <you@example.com>
2023-11-21 20:09:38 +0530

    commit

---
+++ connectwave/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home'
+import '../src/styles.scss'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
